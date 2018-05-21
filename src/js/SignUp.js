1fb2819946110e50be88b01669c7f552d67cd92f
JeongMin Choi <jmchoi1234@hotmail.com>
2018-05-21 14:25:35 +0900

    signup - js id pswd 조건 추가, html 꾸밈

---
+++ src/js/SignUp.js
@@ -0,0 +1,13 @@
+$(document).ready()
+var IDlist = [];
+var ID = document.getElementById("ID").value;
+var password = document.getElementById("password").value;
+if (ID.length < 8){ //ID's length is too short
+    document.getElementById("id_if").innerHTML = "최소 8글자";
+}
+else if(IDlist.includes(ID)){ //ID is used by someone else
+    document.getElementById("id_if").innerHTML = "사용중인 ID";
+}
+if (password.length < 8){ //password's length is too short
+    document.getElementById("pswd_if").innerHTML = "최소 8글자"
+}
