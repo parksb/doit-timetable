067cf4799beaf7ebbe6ca13434721a44e1924049
JeongMin Choi <jmchoi1234@hotmail.com>
2018-07-18 13:06:17 +0900

    newteam - 수정

--- public/newteam.php
+++ public/newteam.php
@@ -2,6 +2,26 @@
 <html>
     <head>
         <title>팀 생성</title>
+        <style>
+            #linklist{
+                float : right;
+                width : 70%;
+            }
+        </style><!--temporary-->
+        <script>
+            function memSearch(){
+                var memberID = document.getElementById("memberID").value;
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function(){
+                    if (this.readyState == 4 && this.status == 200){
+                        document.getElementById("memberlist").innerHTML = this.responseText;
+                    }
+                }
+                xmlhttp.open("GET", "src/php/_memsearch.php?memID=" + memberID, true); //_memsearch.php가 실행되지 않음
+                xmlhttp.send();
+            }
+        </script><!--temporary-->
+    </body>
     </head>
     <body>
         <header>
@@ -12,18 +32,18 @@
             </div>
         </header>
         <hr>
-        <form method = "POST" action = "../src/php/_signup.php">
-            Team Name: <input type = "text" name = "teamName">
-            <div>
-                <h3>팀원 목록</h3>
-                <table id = "memberlist"> <!--memberlist는 json stringify로 db에 저장-->
-                    <tr>
-                        <th>이름</th>
-                        <th>아이디</th>
-                        <th>이메일</th>
-                    </tr>
-                </table>
-            </div>
-        </form>
+        팀 이름: <input type = "text" name = "teamName"><br><br>
+        <div>
+            팀원 아이디를 입력하세요: <input type = "text" id = "memberID">
+            <button onclick = "memSearch()">입력하기</button>
+            <h3>팀원 목록</h3>
+            <table id = "memberlist"> <!--memberlist는 json stringify로 db에 저장-->
+                <tr>
+                    <th>이름</th>
+                    <th>아이디</th>
+                    <th>이메일</th>
+                </tr>
+            </table>
+        </div>
     </body>
 </html>
