31772bc070351a240589f777789b4c275df59234
ParkSB <parkgds@gmail.com>
2018-08-03 14:29:32 +0900

    add team table

--- public/teamtable.php
+++ public/teamtable.php
@@ -3,7 +3,6 @@
     h3{
         margin: 5% 15% 0px;
     }
-
     table, th, td{
         margin: 5% 15% 0px;
         border: 1px solid black;
@@ -12,15 +11,44 @@
         padding: 5px;
     }
 </style>
-<head></head>
+
 <body>
     <h3>Your Team's timetable</h3>
-    <p></p>
+    
     <table width="50%">
             <tr id = "week">
                 <th align="center">시간</th>
             </tr>
-            <tr id = "block"></tr>
+            <tr class = "block">
+                <td align="center">9:00 ~ 10:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">10:00 ~ 11:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">11:00 ~ 12:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">12:00 ~ 13:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">13:00 ~ 14:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">14:00 ~ 15:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">15:00 ~ 16:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">16:00 ~ 17:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">17:00 ~ 18:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">18:00 ~ 19:00</td>
+            </tr>
     </table>
     <script src = "src/teamtable.min.js">
     </script>
--- src/js/teamtable.js
+++ src/js/teamtable.js
@@ -5,19 +5,12 @@ $('#el').on('click', () => {
     $(this).html('clicked');
 });
 
-for(var i = 0; i < 7; i++){
-    $('#blank').append('<td></td>');
-}
-
-for(var i = 9; i < 20; i++){
-    $('#block').append(`
-    <tr id = "blank">
-    <td align="center">${i}:00 ~ ${i+1}:00</td>
-    </tr>`);
+for (var i = 0; i < 7; i++) {
+    $('.block').append(`<td></td>`);
 }
 
 var weekday = ["월", "화", "수", "목", "금", "토", "일"];
 
-for(var i = 0; i < 7; i++){
-    $('week').append(`<th>${weekday[i]}</th>`);
+for (var i = 0; i < 7; i++) {
+    $('#week').append(`<th>${weekday[i]}</th>`);
 }
