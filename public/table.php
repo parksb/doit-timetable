fae9c88808fa738cc61ba48dc3d55cc1eb002307
ParkSB <parkgds@gmail.com>
2018-08-03 15:14:25 +0900

    implement a rendering teamtable

---
+++ public/table.php
@@ -0,0 +1,56 @@
+<html>
+<style>
+    h3{
+        margin: 5% 15% 0px;
+    }
+    table, th, td{
+        margin: 5% 15% 0px;
+        border: 1px solid black;
+    }
+    th, td{
+        padding: 5px;
+    }
+</style>
+
+<body>
+    <h3>Your Team's timetable</h3>
+    
+    <table width="50%">
+            <tr id = "week">
+                <th align="center">시간</th>
+            </tr>
+            <tr class = "block">
+                <td align="center">9:00 ~ 10:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">10:00 ~ 11:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">11:00 ~ 12:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">12:00 ~ 13:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">13:00 ~ 14:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">14:00 ~ 15:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">15:00 ~ 16:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">16:00 ~ 17:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">17:00 ~ 18:00</td>
+            </tr>
+            <tr class = "block">
+                <td align="center">18:00 ~ 19:00</td>
+            </tr>
+    </table>
+    <script src = "src/edit.min.js">
+    </script>
+</body>
+</html>
