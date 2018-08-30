4237a4fd3d652dc4d4966a66f29426c6398fef38
K-osA <evbayooooo@gmail.com>
2018-08-31 00:29:07 +0900

    비밀번호 평문->MD5

--- src/php/_index.php
+++ src/php/_index.php
@@ -6,7 +6,9 @@ session_start();
 $id = $_POST['user_id'];
 $pass = $_POST['user_pass'];
 
-$sql="SELECT*FROM user WHERE user_Id = '$id' AND pass='$pass'";
+$md5pass = md5($pass);
+
+$sql="SELECT*FROM user WHERE user_Id = '$id' AND pass='$md5pass'";
 
 $res=mysqli_query($conn,$sql);
 $count=mysqli_num_rows($res);
@@ -14,6 +16,7 @@ $count=mysqli_num_rows($res);
 if($count==1){
 
     $_SESSION['id']=$id;
+    $_SESSION['pw']=$md5pass;
 
     if(isset($_SESSION['id'])){
         echo "<script>alert(\"login success\");history.back(-1);</script>";
